060f71ac3451348591612d7a4183fb388daf2f30
Sarmad <sshssn@yahoo.com>
2025-09-11 15:08:16 +0000

    increase the logo size pls

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export function Logo() {
   return (
     <svg
-      width="24"
-      height="24"
+      width="32"
+      height="32"
       viewBox="0 0 64 64"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
