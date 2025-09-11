e5c6b50ba4941fddf5dc998d1c321492fbbd6ca7
Sarmad <sshssn@yahoo.com>
2025-09-11 15:13:12 +0000

    redesign the chat icon next to the text based logo and carry it over lik

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,36 +3,19 @@ import React from 'react';
 export function Logo() {
   return (
     <svg
-      width="42"
-      height="42"
-      viewBox="0 0 64 64"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="text-primary dark:text-[#6d28d9]"
+      className="text-primary dark:text-purple-500"
     >
       <path
-        d="M49 10H15C12.2386 10 10 12.2386 10 15V37C10 39.7614 12.2386 42 15 42H24V51.9143C24 53.4796 25.8021 54.425 27.0496 53.483L38.2929 45H49C51.7614 45 54 42.7614 54 40V15C54 12.2386 51.7614 10 49 10Z"
-        fill="currentColor"
-        fillOpacity="0.2"
-      />
-      <g className="stroke-primary-foreground dark:stroke-primary-foreground">
-        <path
-            d="M29.5 24.5L34.5 29.5L29.5 34.5"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        />
-        <path
-            d="M39.5 24.5L44.5 29.5L39.5 34.5"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        />
-      </g>
-      <path
-        d="M49 10H15C12.2386 10 10 12.2386 10 15V37C10 39.7614 12.2386 42 15 42H24V51.9143C24 53.4796 25.8021 54.425 27.0496 53.483L38.2929 45H49C51.7614 45 54 42.7614 54 40V15C54 12.2386 51.7614 10 49 10Z"
+        d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
         stroke="currentColor"
-        strokeWidth="3"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
